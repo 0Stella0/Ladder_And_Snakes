6cfc67787e7819633c832cf2ccd5c97c97fe8e8d
Stella Ivanova <stella._.ivanova@abv.bg>
2024-12-19 00:17:04 +0200

    Updates

--- UP_Practicum_DomashnaRabota1/UP_Practicum_DomashnaRabota1.cpp
+++ UP_Practicum_DomashnaRabota1/UP_Practicum_DomashnaRabota1.cpp
@@ -1,3 +1,4 @@
+//TRUST
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
@@ -26,31 +27,30 @@ void printBoard(int n, char** board, char symbols[], int players, int** snakes, 
     for (int rows = 0; rows < n; rows++) {
         for (int columns = 0; columns < n; columns++) {
             int position = rows * n + columns;
-            bool printed = false; 
+            bool printed = false;
 
-            for (int i = 0; i < players; i++) {
-                if (board[rows][columns] == symbols[i]) {
-                    cout << symbols[i] << " ";
+            for (int i = 0; i < l; i++) {
+                if ((ladders[i][0] - 1) == position) {
+                    cout << "L ";
                     printed = true;
                     break;
                 }
             }
-
             if (!printed) {
-                for (int i = 0; i < l; i++) {
-                    if ((ladders[i][0] - 1) == position) {
-                        cout << "L ";
+                for (int i = 0; i < s; i++) {
+                    if ((snakes[i][0] - 1) == position) {
+                        cout << "S ";
                         printed = true;
                         break;
                     }
                 }
-                if (!printed) {
-                    for (int i = 0; i < s; i++) {
-                        if ((snakes[i][0] - 1) == position) {
-                            cout << "S ";
-                            printed = true;
-                            break;
-                        }
+            }
+            if (!printed) {
+                for (int i = 0; i < players; i++) {
+                    if (board[rows][columns] == symbols[i]) {
+                        cout << symbols[i] << " ";
+                        printed = true;
+                        break;
                     }
                 }
             }
@@ -62,43 +62,26 @@ void printBoard(int n, char** board, char symbols[], int players, int** snakes, 
     }
 }
 
-bool is_a_Win(int n, char** board, char symbols[], int position) {
-    if (position == (n * n - 1)) {
-        return true;
-    }
-    return false;
-}
-
 void boardMovement(int n, char** board, char symbols[], int players, int** snakes, int** ladders, int s, int l) {
-    std::srand(time(0));
     bool end = false;
+    int playersPosition[4] = { 0 };
+
     while (!end) {
         for (int i = 0; i < players; i++) {
             int roll = rollDice();
             cout << "Player " << symbols[i] << " rolled: " << roll << "\n";
-            int position = -1;
-            for (int rows = 0; rows < n; rows++) {
-                for (int columns = 0; columns < n; columns++) {
-                    if (board[rows][columns] == symbols[i]) {
-                        position = rows * n + columns;
-                        board[rows][columns] = ' - ';
-                        break;
-                    }
-                }
-                if (position != -1) {
-                    break; 
+
+            int position_after_roll = playersPosition[i] + roll;
+            if (position_after_roll > n * n - 1)
+                continue;
+           
+            for (int j = 0; j < players; j++) {
+                while (position_after_roll == playersPosition[j] && j != i) {
+                    roll = rollDice();
+                    position_after_roll = playersPosition[i] + roll;
                 }
             }
-
-            int position_after_roll = position + roll;
-
-            if (is_a_Win(n, board, symbols, position_after_roll) == true) {
-                cout << " ~~ " << symbols[i] << " has won the game! gj ~~" << endl;
-                board[position_after_roll / n][position_after_roll % n] = symbols[i];
-                end = true;
-                break;
-            }
-
+           
             for (int j = 0; j < l; j++) {
                 if ((ladders[j][0] - 1) == position_after_roll) {
                     cout << "The player encounter a ladder! " << endl;
@@ -117,24 +100,47 @@ void boardMovement(int n, char** board, char symbols[], int players, int** snake
             int changed_row = position_after_roll / n;
             int changed_column = position_after_roll % n;
             board[changed_row][changed_column] = symbols[i];
+            board[playersPosition[i] / n][playersPosition[i] % n] = '-';
+            playersPosition[i] = position_after_roll;
             printBoard(n, board, symbols, players, snakes, ladders, s, l);
+            
+            if (position_after_roll == (n * n - 1)) {
+                cout << " ~~ " << symbols[i] << " has won the game! gj ~~" << endl;
+                board[n - 1][n - 1] = symbols[i];
+                end = true;
+                break;
+            }
         }
     }
 }
 
-void laddersInput(int a, int** arr, int n) {
+bool edgeCaseSnakeLaddersEndless(int s, int** snakes, int l, int** ladders) {
+    for (int i = 0; i < s; i++) {
+        for (int j = 0; j < l; j++) {
+            if (snakes[i][0] == ladders[i][0]){
+                return false;
+            }
+            if (snakes[i][1] == ladders[j][0]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+void laddersInput(int l, int** ladders, int n) {
     int board_size = (n * n) - 1;
-    if (a > 0) {
-        for (int i = 0; i < a; i++) {
-            arr[i] = new int[2];
+    if (l > 0) {
+        for (int i = 0; i < l; i++) {
+            ladders[i] = new int[2];
             while (true) {
                 cout << "Enter the start and end cell of ladder" << i + 1 << ": ";
-                cin >> arr[i][0];
-                cin >> arr[i][1];
-                if (arr[i][0] < 1 || arr[i][0] > board_size || arr[i][1] < 1 || arr[i][1] > board_size) {
+                cin >> ladders[i][0];
+                cin >> ladders[i][1];
+                if (ladders[i][0] < 2 || ladders[i][0] > board_size - 1 || ladders[i][1] < 2 || ladders[i][1] > board_size - 1) {
                     cerr << "Can't be bigger than the size of the board. Enter again: " << endl;
                 }
-                else if (arr[i][0] > arr[i][1]) {
+                else if (ladders[i][0] > ladders[i][1]) {
                     cerr << "The starting point must me less than the ending point. Enter again: " << endl;
                 }
                 else {
@@ -145,19 +151,19 @@ void laddersInput(int a, int** arr, int n) {
     }
 }
 
-void snakesInput(int a, int** arr, int n) {
+void snakesInput(int s, int** snakes, int n) {
     int board_size = (n * n) - 1;
-    if (a > 0) {
-        for (int i = 0; i < a; i++) {
-            arr[i] = new int[2];
+    if (s > 0) {
+        for (int i = 0; i < s; i++) {
+            snakes[i] = new int[2];
             while (true) {
                 cout << "Enter the start and end cell of snake" << i + 1 << ": ";
-                cin >> arr[i][0];
-                cin >> arr[i][1];
-                if (arr[i][0] < 1 || arr[i][0] > board_size || arr[i][1] < 1 || arr[i][1] > board_size) {
+                cin >> snakes[i][0];
+                cin >> snakes[i][1];
+                if (snakes[i][0] < 2 || snakes[i][0] > board_size - 1 || snakes[i][1] < 2 || snakes[i][1] > board_size - 1) {
                     cerr << "Can't be bigger than the size of the board. Enter again: " << endl;
                 }
-                else if (arr[i][0] < arr[i][1]) {
+                else if (snakes[i][0] < snakes[i][1]) {
                     cerr << "The starting point must me less than the ending point. Enter again: " << endl;
                 }
                 else {
@@ -169,13 +175,13 @@ void snakesInput(int a, int** arr, int n) {
 }
 
 int main() {
+    std::srand(time(0));
     int n;
     cout << "Enter the size of the board (NxN): ";
     cin >> n;
     if ((!(n % 5 == 0)) || n > 15) {
         cerr << "  ~The board can only be sizes 5x5/10x10/15x15." << endl;
         return 1;
-
     }
 
     int players;
@@ -210,8 +216,7 @@ int main() {
             board[rows][columns] = '-';
         }
     }
-
-
+        
     int ladders;
     cout << "Enter the number of ladders: ";
     cin >> ladders;
@@ -223,6 +228,16 @@ int main() {
     cin >> snakes;
     int** snakes_coordinates = new int* [snakes];
     snakesInput(snakes, snakes_coordinates, n);
+
+    if (!edgeCaseSnakeLaddersEndless(snakes, snakes_coordinates, ladders, ladders_coordinates)) {
+        cerr << "The snakes and ladders cannot be the same or create endless rotations.";
+        return 0;
+    }
+
+   
+   /*for (int i = 0; i < players; i++) {
+        board[0][0] == symbols[i];
+    }*/
 
     printBoard(n, board, symbols, players, snakes_coordinates, ladders_coordinates, snakes, ladders);
     boardMovement(n, board, symbols, players, snakes_coordinates, ladders_coordinates, snakes, ladders);
@@ -244,3 +259,5 @@ int main() {
 
     return 0;
 }
+
+//Samo ne mi izkarva v nachaloto vsichki characters na [0,0]
